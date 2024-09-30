fdfcfa149229c936f4fcfc5d6da144e17d7cb7cf
AdamFuzesi <adamfuzeri@gmail.com>
2024-09-30 15:46:52 -0300

    final commit

--- matrix.py
+++ matrix.py
@@ -9,26 +9,33 @@ MAX_STREAM_LENGTH = 14
 PAUSE = 0.1
 STREAM_CHARS = ['0', '1']
 # represents the Matrix based code stream effect
+# can change based on preference
 
 DENSITY = 0.02
 
 WIDTH = shutil.get_terminal_size()[0]
 WIDTH -= 1
 
-print("Input Ctrl-C to exit process")
+print("Input Ctrl-C to exit the process")
 time.sleep(2)
 
 try:
     columns = [0] * WIDTH
+
     while True:
+
         for i in range(WIDTH):
+
             if columns[i] == 0:
+
                 if random.random() <= DENSITY:
                     columns[i] = random.randint(MIN_STREAM_LENGTH, MAX_STREAM_LENGTH)
-
+                    
             if columns[i] > 0:
+
                 print(Fore.LIGHTGREEN_EX + random.choice(STREAM_CHARS), end='')
                 columns[i] -= 1
+
             else:
                 print(' ', end='')
 
